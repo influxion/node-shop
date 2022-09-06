90c536ea2d3a81379d0e7e8482a3ab9c9878d005
influxion <nicholsjordon721@gmail.com>
2022-09-05 19:05:08 -0500

    Sanitation fix

--- validators/admin.js
+++ validators/admin.js
@@ -3,13 +3,13 @@ const { body } = require('express-validator');
 exports.postAddProduct = [
   body('title').isString().isLength({ min: 3 }).trim(),
   body('imageUrl').isURL(),
-  body('price').isFloat(),
+  body('price').isFloat().isDecimal({ decimal_digits: '2,2' }),
   body('description').isLength({ min: 3, max: 255 }).trim(),
 ];
 
 exports.postEditProduct = [
   body('title').isString().isLength({ min: 3 }).trim(),
   body('imageUrl').isURL(),
-  body('price').isFloat(),
+  body('price').isFloat().isDecimal({ decimal_digits: '2,2' }),
   body('description').isLength({ min: 3, max: 255 }).trim(),
 ];
